e1271905ffacb93818ce11672da63298412d331d
Drombeys <dima.zolotov02@gmail.com>
2022-06-08 00:49:00 +0300

    Add empty WPF project

---
+++ src/StalkerBelarus.Launcher/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace StalkerBelarus.Launcher;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
