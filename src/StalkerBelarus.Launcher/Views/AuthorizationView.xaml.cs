8046942234d632659285ffb639709200431cf7a5
Drombeys <dima.zolotov02@gmail.com>
2022-06-08 18:20:48 +0300

    Add authorization view

---
+++ src/StalkerBelarus.Launcher/Views/AuthorizationView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace StalkerBelarus.Launcher.Views;
+
+/// <summary>
+/// Логика взаимодействия для AuthorizationView.xaml
+/// </summary>
+public partial class AuthorizationView : UserControl
+{
+    public AuthorizationView()
+    {
+        InitializeComponent();
+    }
+}
