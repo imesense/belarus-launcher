312108e0405ad45cc627317f0096a97efebecab2
Drombeys <dima.zolotov02@gmail.com>
2022-06-10 18:58:31 +0300

    Add menu view

---
+++ src/StalkerBelarus.Launcher/Views/MenuView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace StalkerBelarus.Launcher.Views;
+
+/// <summary>
+/// Логика взаимодействия для MenuView.xaml
+/// </summary>
+public partial class MenuView : UserControl
+{
+    public MenuView()
+    {
+        InitializeComponent();
+    }
+}
