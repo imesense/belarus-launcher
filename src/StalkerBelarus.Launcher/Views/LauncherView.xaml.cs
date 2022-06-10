1667dd764252bd3d233d0cca9c009a6120ba6727
Drombeys <dima.zolotov02@gmail.com>
2022-06-10 19:03:07 +0300

    Add launcher view

---
+++ src/StalkerBelarus.Launcher/Views/LauncherView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace StalkerBelarus.Launcher.Views;
+
+/// <summary>
+/// Логика взаимодействия для LauncherView.xaml
+/// </summary>
+public partial class LauncherView : UserControl
+{
+    public LauncherView()
+    {
+        InitializeComponent();
+    }
+}
