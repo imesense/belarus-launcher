7fdce0ff94631138fe34987412f141c8a377487d
Drombeys <dima.zolotov02@gmail.com>
2022-06-10 18:27:00 +0300

    Add news view

---
+++ src/StalkerBelarus.Launcher/Views/NewsView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace StalkerBelarus.Launcher.Views;
+
+/// <summary>
+/// Логика взаимодействия для NewsView.xaml
+/// </summary>
+public partial class NewsView : UserControl
+{
+    public NewsView()
+    {
+        InitializeComponent();
+    }
+}
