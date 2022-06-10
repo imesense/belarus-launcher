a4a70fb70e26061f873043a9f80545ad7572a7d1
Drombeys <dima.zolotov02@gmail.com>
2022-06-10 18:36:42 +0300

    Add news slider view

---
+++ src/StalkerBelarus.Launcher/Views/NewsSliderView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace StalkerBelarus.Launcher.Views;
+
+/// <summary>
+/// Логика взаимодействия для NewsSliderView.xaml
+/// </summary>
+public partial class NewsSliderView : UserControl
+{
+    public NewsSliderView()
+    {
+        InitializeComponent();
+    }
+}
