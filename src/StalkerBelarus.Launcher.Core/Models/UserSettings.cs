5cabfa0a561d2ed7276f65fb95dc04bac756b018
Drombeys <dima.zolotov02@gmail.com>
2023-07-03 19:37:25 +0300

    Fix saving user settings

--- src/StalkerBelarus.Launcher.Core/Models/UserSettings.cs
+++ src/StalkerBelarus.Launcher.Core/Models/UserSettings.cs
@@ -1,6 +1,6 @@
 namespace StalkerBelarus.Launcher.Core.Models;
 
 public class UserSettings {
-    public string? UserName { get; set; }
-    public string? IpAdress { get; set; }
+    public string UserName { get; set; } = string.Empty;
+    public string IpAdress { get; set; } = string.Empty;
 }
