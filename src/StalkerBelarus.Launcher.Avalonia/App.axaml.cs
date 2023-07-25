031132dd1328e22c7f31afc58a72ed82c22657b3
Drombeys <dima.zolotov02@gmail.com>
2023-07-25 23:39:12 +0300

    Integrate logging

--- src/StalkerBelarus.Launcher.Avalonia/App.axaml.cs
+++ src/StalkerBelarus.Launcher.Avalonia/App.axaml.cs
@@ -3,6 +3,9 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using Serilog;
 
 using StalkerBelarus.Launcher.Avalonia.Views;
 
@@ -12,17 +15,29 @@ public partial class App : Application {
     private readonly IServiceProvider _serviceProvider;
 
     public App() {
-        _serviceProvider = ConfigureServices().BuildServiceProvider();
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.File(@"_user_/logs/sblauncher_report.txt", rollingInterval: RollingInterval.Infinite)
+            .CreateLogger();
+        
+        _serviceProvider = ConfigureServices()
+            .BuildServiceProvider();
     }
     
     private IServiceCollection ConfigureServices() {
         var services = new ServiceCollection();
-
+        services.AddLogging(loggingBuilder =>
+            loggingBuilder.AddSerilog(dispose: true));
         return services;
     }
-
+    
     public override void Initialize() {
-        AvaloniaXamlLoader.Load(this);
+        var logger = _serviceProvider.GetRequiredService<ILogger<App>>();
+        try {
+            AvaloniaXamlLoader.Load(this);
+        } catch (Exception exception) {
+            logger.LogCritical("{Message}", exception.Message);
+            throw;
+        }
     }
 
     public override void OnFrameworkInitializationCompleted() {
