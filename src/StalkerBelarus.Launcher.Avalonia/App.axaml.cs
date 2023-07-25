366057678af98814a93d53210c14ba0b2c6b05a1
Drombeys <dima.zolotov02@gmail.com>
2023-07-25 22:55:05 +0300

    Integrate Microsoft dependency injection

--- src/StalkerBelarus.Launcher.Avalonia/App.axaml.cs
+++ src/StalkerBelarus.Launcher.Avalonia/App.axaml.cs
@@ -2,11 +2,25 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
+using Microsoft.Extensions.DependencyInjection;
+
 using StalkerBelarus.Launcher.Avalonia.Views;
 
 namespace StalkerBelarus.Launcher.Avalonia;
 
 public partial class App : Application {
+    private readonly IServiceProvider _serviceProvider;
+
+    public App() {
+        _serviceProvider = ConfigureServices().BuildServiceProvider();
+    }
+    
+    private IServiceCollection ConfigureServices() {
+        var services = new ServiceCollection();
+
+        return services;
+    }
+
     public override void Initialize() {
         AvaloniaXamlLoader.Load(this);
     }
