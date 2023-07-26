0a5147cc835c1060481ffd1e2d380befbdcdf655
Drombeys <dima.zolotov02@gmail.com>
2023-07-26 17:07:52 +0300

    Implement application version output

--- src/StalkerBelarus.Launcher.Avalonia/ViewModels/LauncherViewModel.cs
+++ src/StalkerBelarus.Launcher.Avalonia/ViewModels/LauncherViewModel.cs
@@ -1,11 +1,19 @@
 using Microsoft.Extensions.Logging;
 
+using StalkerBelarus.Launcher.Avalonia.Helpers;
+
 namespace StalkerBelarus.Launcher.Avalonia.ViewModels;
 
 public class LauncherViewModel : ViewModelBase {
     private readonly ILogger<LauncherViewModel> _logger;
+    
+    public string AppVersion { get; set; }
+    public string CompanyName { get; set; }
 
     public LauncherViewModel(ILogger<LauncherViewModel> logger) {
         _logger = logger;
+        
+        AppVersion = ApplicationHelper.GetAppVersion();
+        CompanyName = (char)0169 + ApplicationHelper.GetCompanyName();
     }
 }
