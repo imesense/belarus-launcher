b22c812585bb6ed591edc77def2afddec4952c6f
Drombeys <dima.zolotov02@gmail.com>
2023-08-01 12:29:21 +0300

    Implement getting json data from github release

--- src/StalkerBelarus.Launcher.Core/Services/GitHubApiService.cs
+++ src/StalkerBelarus.Launcher.Core/Services/GitHubApiService.cs
@@ -16,9 +16,27 @@ public class GitHubApiService : IGitHubApiService {
         _httpClient = httpClient;
     }
 
+    /// <summary>
+    /// Downloads a JSON file from a GitHub release and deserializes it into the specified object type
+    /// </summary>
+    /// <typeparam name="T">The type of the object to deserialize the JSON into</typeparam>
+    /// <param name="filename">The name of the JSON file to download</param>
+    /// <returns>The deserialized object of type T if successful, or null if the file is not found or deserialization fails</returns>
+    public async Task<T?> DownloadJsonAsync<T>(string filename) where T : class {
+        // Get the GitHub release information
+        var release = await GetGitHubReleaseAsync();
+        // Find the asset with the specified filename
+        var asset = release?.Assets?.FirstOrDefault(n => n.Name.Equals(filename));
+        // Download the asset as a stream
+        await using var assetStream = await _httpClient.GetStreamAsync(asset?.BrowserDownloadUrl);
+        // Deserialize the JSON content into the specified object type
+        return await JsonSerializer.DeserializeAsync<T>(assetStream);
+    }
 
     public async Task<GitHubRelease?> GetGitHubReleaseAsync() {
+        // Download the asset as a stream.
         await using var response = await _httpClient.GetStreamAsync(_httpClient.BaseAddress);
+        // Deserialize the JSON content into the specified object type
         return await JsonSerializer.DeserializeAsync<GitHubRelease>(response);
     }
 
--- src/StalkerBelarus.Launcher.Core/Services/IGitHubApiService.cs
+++ src/StalkerBelarus.Launcher.Core/Services/IGitHubApiService.cs
@@ -3,5 +3,6 @@ using StalkerBelarus.Launcher.Core.Models;
 namespace StalkerBelarus.Launcher.Core.Services;
 
 public interface IGitHubApiService {
+    Task<T?> DownloadJsonAsync<T>(string filename) where T : class;
     Task<GitHubRelease?> GetGitHubReleaseAsync();
 }
