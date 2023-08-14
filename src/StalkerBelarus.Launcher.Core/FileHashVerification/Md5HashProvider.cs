b298fbac5e0c9ef417dc5a3b7a155d422c2143e9
Drombeys <dima.zolotov02@gmail.com>
2023-08-14 20:29:10 +0300

    Implement `CryptoHasher` app

--- src/StalkerBelarus.Launcher.Core/FileHashVerification/Md5HashProvider.cs
+++ src/StalkerBelarus.Launcher.Core/FileHashVerification/Md5HashProvider.cs
@@ -5,9 +5,9 @@ using Microsoft.Extensions.Logging;
 namespace StalkerBelarus.Launcher.Core.FileHashVerification;
 
 public class Md5HashProvider : IHashProvider {
-    private readonly ILogger<Md5HashProvider> _logger;
+    private readonly ILogger<Md5HashProvider>? _logger;
 
-    public Md5HashProvider(ILogger<Md5HashProvider> logger) {
+    public Md5HashProvider(ILogger<Md5HashProvider>? logger) {
         _logger = logger;
     }
 
@@ -16,10 +16,10 @@ public class Md5HashProvider : IHashProvider {
             using var md5 = MD5.Create();
             await using var stream = File.OpenRead(filePath);
             var hashBytes = await md5.ComputeHashAsync(stream, cancellationToken);
-            _logger.LogInformation("Hash {FileName} - {HashBytes}", Path.GetFileName(filePath), hashBytes);
+            _logger?.LogInformation("Hash {FileName} - {HashBytes}", Path.GetFileName(filePath), hashBytes);
             return BitConverter.ToString(hashBytes).Replace("-", "");
         } catch (Exception exception) {
-            _logger.LogError("{Message}", exception.Message);
+            _logger?.LogError("{Message}", exception.Message);
 
             return string.Empty;
         }
---
+++ src/StalkerBelarus.Launcher.CryptoHasher/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+using StalkerBelarus.Launcher.Core;
+using StalkerBelarus.Launcher.Core.FileHashVerification;
+using StalkerBelarus.Launcher.Core.Models;
+
+using static StalkerBelarus.Launcher.Core.FileLocations;
+
+string[] GetDirectories() {
+    return new[] { BinariesDirectory, ResourcesDirectory, PatchesDirectory };
+}
+
+var hashing = new Md5HashProvider(null);
+var folders = GetDirectories();
+var gameResources = new List<GameResource>();
+
+foreach (var folderPath in folders)  {
+    var files = Directory.GetFiles(folderPath);
+    foreach (var filePath in files)
+    {
+        var fileInfo = new FileInfo(filePath);
+        var file = new GameResource { 
+            Title = fileInfo.Name,
+            Directory = fileInfo.Directory!.Name,
+            Hash = await hashing.CalculateHashAsync(filePath)
+        };
+        gameResources.Add(file);
+    }
+}
+
+var options = new JsonSerializerOptions {
+    AllowTrailingCommas = true,
+    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+    WriteIndented = true
+};
+await using var fs = new FileStream(FileNamesStorage.HashResources, FileMode.OpenOrCreate);
+await JsonSerializer.SerializeAsync(fs, gameResources, options);
+using var reader = new StreamReader(fs);
+
+Console.WriteLine(File.ReadAllText(FileNamesStorage.HashResources));
+
