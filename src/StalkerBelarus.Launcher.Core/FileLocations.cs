639451a783737bd8946bf1ef254f79f9fa7384b7
Drombeys <dima.zolotov02@gmail.com>
2023-11-15 23:15:06 +0300

    Rename log folder

--- src/StalkerBelarus.Launcher.Core/FileLocations.cs
+++ src/StalkerBelarus.Launcher.Core/FileLocations.cs
@@ -6,6 +6,6 @@ public static class FileLocations {
     public static string ResourcesDirectory => Path.Combine(BaseDirectory, "resources");
     public static string PatchesDirectory => Path.Combine(BaseDirectory, "patches");
     public static string UserDirectory => Path.Combine(BaseDirectory, "_user_");
-    public static string LogsDirectory => Path.Combine(UserDirectory, "_logs_");
+    public static string LogsDirectory => Path.Combine(UserDirectory, "logs");
     public static string UserSettingPath => Path.Combine(UserDirectory, "sblauncher.json");
 }
