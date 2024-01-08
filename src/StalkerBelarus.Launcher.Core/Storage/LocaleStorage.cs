b3ba0189e5ee78de5cb507540b505749acd1480d
Drombeys <dima.zolotov02@gmail.com>
2024-01-09 00:09:55 +0300

    Delete unused locale

--- src/StalkerBelarus.Launcher.Core/Storage/LocaleStorage.cs
+++ src/StalkerBelarus.Launcher.Core/Storage/LocaleStorage.cs
@@ -1,4 +1,4 @@
-﻿using StalkerBelarus.Launcher.Core.Models;
+using StalkerBelarus.Launcher.Core.Models;
 
 namespace StalkerBelarus.Launcher.Core.Storage;
 
@@ -7,7 +7,7 @@ public class LocaleStorage : ILocaleStorage {
     public IList<Locale> GetLocales() {
         return new List<Locale> {
             new() { Key = "ru", Title = "Русский", },
-            new() { Key = "be", Title = "Беларуская", },
+            // new() { Key = "be", Title = "Беларуская", },
             new() { Key = "en", Title = "English", },
         };
     }
