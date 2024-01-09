aef5c9bd71ef3f8a6d526561cb71c87fb794ddc2
Drombeys <dima.zolotov02@gmail.com>
2024-01-09 13:36:52 +0300

    Implement news in different languages

--- src/StalkerBelarus.Launcher.Avalonia/ViewModels/NewsSliderViewModel.cs
+++ src/StalkerBelarus.Launcher.Avalonia/ViewModels/NewsSliderViewModel.cs
@@ -8,6 +8,7 @@ using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
 using StalkerBelarus.Launcher.Core;
+using StalkerBelarus.Launcher.Core.Manager;
 using StalkerBelarus.Launcher.Core.Models;
 using StalkerBelarus.Launcher.Core.Services;
 
@@ -16,6 +17,7 @@ namespace StalkerBelarus.Launcher.Avalonia.ViewModels;
 public class NewsSliderViewModel : ReactiveObject, IAsyncInitialization {
     private readonly ILogger<NewsSliderViewModel> _logger;
     private readonly IGitStorageApiService _newsService;
+    private readonly UserManager _userManager;
 
     [Reactive] public int NumPage { get; set; }
     [Reactive] public NewsViewModel? SelectedNewsViewModel { get; private set; }
@@ -25,11 +27,12 @@ public class NewsSliderViewModel : ReactiveObject, IAsyncInitialization {
     public ReactiveCommand<Unit, Unit> GoNext { get; set; } = null!;
     public ReactiveCommand<Unit, Unit> GoBack { get; set; } = null!;
     
-    public NewsSliderViewModel(ILogger<NewsSliderViewModel> logger, IGitStorageApiService newsService, LinkViewModel linkViewModel) {
+    public NewsSliderViewModel(ILogger<NewsSliderViewModel> logger, IGitStorageApiService newsService, LinkViewModel linkViewModel,
+        UserManager userManager) {
         _logger = logger;
         _newsService = newsService;
         LinkViewModel = linkViewModel;
-        
+        _userManager = userManager;
         SetupBinding();
         SetupCommands();
         Initialization = InitializeAsync();
@@ -39,14 +42,23 @@ public class NewsSliderViewModel : ReactiveObject, IAsyncInitialization {
     public NewsSliderViewModel() {
         _logger = null!;
         _newsService = null!;
-
+        _userManager = null!;
         LinkViewModel = null!;
         Initialization = null!;
     }
 #endif
 
     private async Task LoadNews() {
-        await foreach (var content in _newsService.DownloadJsonArrayAsync<NewsContent>("news_content.json")) {
+        if (_userManager is null) {
+            throw new NullReferenceException("User manager object is null");
+        }
+        if (_userManager.UserSettings is null) {
+            throw new NullReferenceException("User settings object is null");
+        }
+
+        var locale = _userManager.UserSettings.Locale;
+        var news = _newsService.DownloadJsonArrayAsync<NewsContent>($"news_content_{locale}.json");
+        await foreach (var content in news) {
             News.Add(new NewsViewModel(content!.Title, content.Description));
         }
         
