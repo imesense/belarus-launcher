bc4782a9dd41d56e84610507fe97180fdffc0a89
Drombeys <dima.zolotov02@gmail.com>
2024-01-13 09:40:13 +0300

    Fix log output in `CryptoHasher` project

--- src/ImeSense.Launchers.Belarus.CryptoHasher/Program.cs
+++ src/ImeSense.Launchers.Belarus.CryptoHasher/Program.cs
@@ -11,33 +11,42 @@ IEnumerable<string> GetDirectories() {
     return new[] { BinariesDirectory, ResourcesDirectory, PatchesDirectory };
 }
 
-var hashing = new Md5HashProvider(null);
-var folders = GetDirectories();
-var gameResources = new List<GameResource>();
+try {
+    var hashing = new Md5HashProvider(null);
+    var folders = GetDirectories();
+    var gameResources = new List<GameResource>();
 
-foreach (var folderPath in folders)  {
-    var files = Directory.GetFiles(folderPath);
-    foreach (var filePath in files)
-    {
-        var fileInfo = new FileInfo(filePath);
-
-        await using var stream = File.OpenRead(filePath);
-        var file = new GameResource { 
-            Title = fileInfo.Name,
-            Directory = fileInfo.Directory!.Name,
-            Hash = await hashing.CalculateHashAsync(stream)
-        };
-        gameResources.Add(file);
+    foreach (var folderPath in folders) {
+        var files = Directory.GetFiles(folderPath);
+        foreach (var filePath in files) {
+            
+            var fileInfo = new FileInfo(filePath);
+            Console.WriteLine($"Calculating hash for file {fileInfo.Name}");
+            await using var stream = File.OpenRead(filePath);
+            var file = new GameResource {
+                Title = fileInfo.Name,
+                Directory = fileInfo.Directory!.Name,
+                Hash = await hashing.CalculateHashAsync(stream)
+            };
+            gameResources.Add(file);
+        }
     }
+
+    var options = new JsonSerializerOptions {
+        AllowTrailingCommas = true,
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+        WriteIndented = true
+    };
+    await using var fs = new FileStream(FileNamesStorage.HashResources, FileMode.OpenOrCreate);
+    await JsonSerializer.SerializeAsync(fs, gameResources, options);
+    fs.Close();
+
+    Console.WriteLine(File.ReadAllText(FileNamesStorage.HashResources));
+} catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+    Console.WriteLine(ex.StackTrace);
+
+    Console.ReadLine();
 }
 
-var options = new JsonSerializerOptions {
-    AllowTrailingCommas = true,
-    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
-    WriteIndented = true
-};
-await using var fs = new FileStream(FileNamesStorage.HashResources, FileMode.OpenOrCreate);
-await JsonSerializer.SerializeAsync(fs, gameResources, options);
-using var reader = new StreamReader(fs);
 
-Console.WriteLine(File.ReadAllText(FileNamesStorage.HashResources));
