5a7c27fcdb700198de2b6616097cb262c386db34
Drombeys <dima.zolotov02@gmail.com>
2024-03-01 08:41:31 +0300

    Improve multithreaded file hash processing in CryptoHasher project

--- src/ImeSense.Launchers.Belarus.Core/FileHashVerification/Md5HashProvider.cs
+++ src/ImeSense.Launchers.Belarus.Core/FileHashVerification/Md5HashProvider.cs
@@ -4,17 +4,26 @@ using Microsoft.Extensions.Logging;
 
 namespace ImeSense.Launchers.Belarus.Core.FileHashVerification;
 
-public class Md5HashProvider : IHashProvider {
-    private readonly ILogger<Md5HashProvider>? _logger;
-
-    public Md5HashProvider(ILogger<Md5HashProvider>? logger) {
-        _logger = logger;
-    }
+public class Md5HashProvider(ILogger<Md5HashProvider>? logger) : IHashProvider {
+    private readonly ILogger<Md5HashProvider>? _logger = logger;
 
     public async Task<string> CalculateHashAsync(Stream stream, CancellationToken cancellationToken = default) {
         try {
             using var md5 = MD5.Create();
             var hashBytes = await md5.ComputeHashAsync(stream, cancellationToken);
+
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        } catch (Exception exception) {
+            _logger?.LogError("{Message}", exception.Message);
+
+            return string.Empty;
+        }
+    }
+
+    public string CalculateHash(Stream stream) {
+        try {
+            using var md5 = MD5.Create();
+            var hashBytes = md5.ComputeHash(stream);
 
             return BitConverter.ToString(hashBytes).Replace("-", "");
         } catch (Exception exception) {
--- src/ImeSense.Launchers.Belarus.CryptoHasher/Program.cs
+++ src/ImeSense.Launchers.Belarus.CryptoHasher/Program.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Logging;
 
 using Serilog;
 
-System.Console.Title = "Belarus CryptoHasher";
+Console.Title = "Belarus CryptoHasher";
 
 IEnumerable<string> GetDirectories() => [DirectoryStorage.Binaries, DirectoryStorage.Resources, DirectoryStorage.Patches];
 
@@ -23,25 +23,28 @@ logger.LogInformation("Start CryptoHasher application");
 var hashing = new Md5HashProvider(factory.CreateLogger<Md5HashProvider>());
 
 try {
+    var gameResourceTasks = new List<Task<GameResource>>();
+    var gameResources = new List<GameResource>();
+    
     var stopwatch = new Stopwatch();
     stopwatch.Start();
-
-    var folders = GetDirectories();
-    var gameResourceTasks = new List<Task<GameResource>>();
-
-    foreach (var folderPath in folders) {
-        var files = Directory.GetFiles(folderPath);
-        foreach (var filePath in files) {
-            gameResourceTasks.Add(AddGameResource(new FileInfo(filePath)));
+    foreach (var folderPath in GetDirectories()) {
+        var dir = new DirectoryInfo(folderPath);
+        foreach (var file in dir.GetFiles()) {
+            if (file.Length > 100000000) {
+                gameResourceTasks.Add(AddGameResourceAsync(file));
+            } else {
+                gameResources.Add(AddGameResource(file));
+            }
         }
     }
 
-    var gameResources = await Task.WhenAll(gameResourceTasks);
+    gameResources.AddRange(await Task.WhenAll(gameResourceTasks));
     stopwatch.Stop();
-    logger.LogInformation("Hash calculation time: {time}", stopwatch.ElapsedMilliseconds);
+    logger.LogInformation("Hash calculation time: {time}mc", stopwatch.ElapsedMilliseconds);
 
-    await using var fs = new FileStream(FileNameStorage.HashResources, FileMode.OpenOrCreate);
-    await JsonSerializer.SerializeAsync(fs, gameResources, SourceGenerationContext.Default.GameResourceArray);
+    await using var fs = new FileStream(FileNameStorage.HashResources, FileMode.CreateNew);
+    await JsonSerializer.SerializeAsync(fs, gameResources.ToArray(), SourceGenerationContext.Default.GameResourceArray);
     fs.Close();
 
     logger.LogInformation("{json}", File.ReadAllText(FileNameStorage.HashResources));
@@ -54,14 +57,36 @@ try {
     Log.CloseAndFlush();
 }
 
-async Task<GameResource> AddGameResource(FileInfo fileInfo) {
+async Task<GameResource> AddGameResourceAsync(FileInfo fileInfo) {
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+
     await using var stream = File.OpenRead(fileInfo.FullName);
     var hashFile = await hashing.CalculateHashAsync(stream);
-    logger.LogInformation("File {FileName} {Length} Kb ({HashBytes})", fileInfo.Name, stream.Length / 1000.0f, hashFile);
+    logger.LogInformation("File {FileName} {Length}Kb Time:{time}ms ({HashBytes}) ", 
+        fileInfo.Name, stream.Length / 1000, stopwatch.ElapsedMilliseconds, hashFile);
+    stopwatch.Stop();
 
     return new GameResource {
         Title = fileInfo.Name,
         Directory = fileInfo.Directory!.Name,
-        Hash = await hashing.CalculateHashAsync(stream)
+        Hash = hashFile
+    };
+}
+
+GameResource AddGameResource(FileInfo fileInfo) {
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    using var stream = File.OpenRead(fileInfo.FullName);
+    var hashFile = hashing.CalculateHash(stream);
+    logger.LogInformation("File {FileName} {Length}Kb Time:{time}ms ({HashBytes}) ", 
+        fileInfo.Name, stream.Length / 1000, stopwatch.ElapsedMilliseconds, hashFile);
+    stopwatch.Stop();
+
+    return new GameResource {
+        Title = fileInfo.Name,
+        Directory = fileInfo.Directory!.Name,
+        Hash = hashFile
     };
 }
