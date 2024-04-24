24a1860f2ae96817afb5a77e1cc7baf4609f8135
Drombeys <dima.zolotov02@gmail.com>
2024-04-24 12:21:08 +0300

    Add `SplashScreenViewModel` in `ViewModelLocator` class

--- src/ImeSense.Launchers.Belarus.Avalonia/Services/ViewModelLocator.cs
+++ src/ImeSense.Launchers.Belarus.Avalonia/Services/ViewModelLocator.cs
@@ -16,4 +16,5 @@ public class ViewModelLocator(IServiceProvider serviceProvider)
     public LinkViewModel LinkViewModel => _serviceProvider.GetRequiredService<LinkViewModel>();
     public NewsSliderViewModel NewsSliderViewModel => _serviceProvider.GetRequiredService<NewsSliderViewModel>();
     public StartGameViewModel StartGameViewModel => _serviceProvider.GetRequiredService<StartGameViewModel>();
+    public SplashScreenViewModel SplashScreenViewModel => _serviceProvider.GetRequiredService<SplashScreenViewModel>();
 }
